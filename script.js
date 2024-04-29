f15993e75fd84fed7440b9095ebeaf5ce66adaa4
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-04-29 18:11:04 +0900

    Add getComputerChoice() function
    Added a function that lets the computer randomly
    pick between rock, scissors or paper

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello Worlddddd')
+function getComputerChoice(){
+    let result=Math.floor(Math.random() *3) 
+    let choice
+    if (result === 0){
+        choice = 'rock'
+    }
+    else if (result === 1){
+        choice = 'scissors'
+    }
+    else {
+        choice = 'paper'
+    }
+    return choice
+}
+
+console.log(getComputerChoice())
