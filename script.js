e5fd949321095458951c903b9effb0c6b1575497
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-04-30 09:21:48 +0900

    Add the rest of functions for the 5 round game

--- script.js
+++ script.js
@@ -1,16 +1,118 @@
-function getComputerChoice(){
-    let result=Math.floor(Math.random() *3) 
-    let choice
-    if (result === 0){
-        choice = 'rock'
-    }
-    else if (result === 1){
-        choice = 'scissors'
-    }
-    else {
-        choice = 'paper'
-    }
-    return choice
+let humanScore = 0;
+let computerScore = 0;
+function getComputerChoice() {
+	let result = Math.floor(Math.random() * 3);
+
+	if (result === 0) {
+		return "rock";
+	} else if (result === 1) {
+		return "scissors";
+	} else {
+		return "paper";
+	}
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+	let choice = prompt("Choose rock, scissors, or paper: ");
+	return choice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+	if (humanChoice == "rock") {
+		if (computerChoice == "rock") {
+			console.log(
+				"You tie!\nYour Score: " +
+					humanScore +
+					"\nComputer Score: " +
+					computerScore
+			);
+		} else if (computerChoice == "scissors") {
+			humanScore++;
+			console.log(
+				"You win!\nYour Score: " +
+					humanScore +
+					"\nComputer Score: " +
+					computerScore
+			);
+		} else {
+			computerScore++;
+			console.log(
+				"You lose!\nYour Score: " +
+					humanScore +
+					"\nComputer Score: " +
+					computerScore
+			);
+		}
+	} else if (humanChoice == "scissors") {
+		if (computerChoice == "rock") {
+			computerScore++;
+			console.log(
+				"You lose!\nYour Score: " +
+					humanScore +
+					"\nComputer Score: " +
+					computerScore
+			);
+		} else if (computerChoice == "scissors") {
+			console.log(
+				"You tie!\nYour Score: " +
+					humanScore +
+					"\nComputer Score: " +
+					computerScore
+			);
+		} else {
+			humanScore++;
+			console.log(
+				"You win!\nYour Score: " +
+					humanScore +
+					"\nComputer Score: " +
+					computerScore
+			);
+		}
+	} else {
+		if (computerChoice == "rock") {
+			humanScore++;
+			return (
+				"You win!\nYour Score: " +
+				humanScore +
+				"\nComputer Score: " +
+				computerScore
+			);
+		} else if (computerChoice == "scissors") {
+			computerScore++;
+			return (
+				"You lose!\nYour Score: " +
+				humanScore +
+				"\nComputer Score: " +
+				computerScore
+			);
+		} else {
+			return (
+				"You tie!\nYour Score: " +
+				humanScore +
+				"\nComputer Score: " +
+				computerScore
+			);
+		}
+	}
+}
+// it is working fine for one game, but after the first iteration of the game, it only produces the same results 4 more times.
+// there are no more prompts for the humanchoice after the first one
+//scoring tally works fine, I think there is a problem with the loop
+function playGame() {
+	while (true) {
+		let humanSelection = getHumanChoice();
+		let computerSelection = getComputerChoice();
+		playRound(humanSelection, computerSelection);
+		if ((humanScore || computerScore) == 5) {
+			if (humanScore > computerScore) {
+				console.log("You win the game!");
+				break;
+			} else {
+				console.log("You lose the game!");
+				break;
+			}
+		}
+	}
+}
+
+playGame();
